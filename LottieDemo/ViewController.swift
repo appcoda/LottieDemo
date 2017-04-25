fe694cd60c0e27423922a8cf636567c9d3b7be3a
Simon Ng <simon@Simons-MacBook-Pro-2.local>
2017-04-25 18:09:09 +0800

    Initial version

--- LottieDemo/ViewController.swift
+++ LottieDemo/ViewController.swift
@@ -7,12 +7,34 @@
 //
 
 import UIKit
+import Lottie
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        if let animationView = LOTAnimationView(name: "servishero_loading") {
+//        if let animationView = LOTAnimationView(contentsOf: URL(string: "https://github.com/airbnb/lottie-ios/raw/master/Example/Assets/LottieLogo1.json")!) {
+            animationView.frame = CGRect(x: 0, y: 0, width: 400, height: 400)
+            animationView.center = self.view.center
+            
+            animationView.loopAnimation = true
+            animationView.contentMode = .scaleAspectFill
+            animationView.animationSpeed = 0.5
+            
+            // Applying UIView animation
+            let minimizeTransform = CGAffineTransform(scaleX: 0.1, y: 0.1)
+            animationView.transform = minimizeTransform
+            UIView.animate(withDuration: 3.0, delay: 0.0, options: [.repeat, .autoreverse], animations: {
+                animationView.transform = CGAffineTransform.identity
+            }, completion: nil)
+            
+            view.addSubview(animationView)
+        
+            animationView.play()
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
